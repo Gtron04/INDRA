68aa4056792f2801b7cb1be5d1f113f6faa32305
sistemas-aux <DELL@SISTEMAS-AUX.PROMOCOSTA>
2022-05-13 11:26:30 -0500

    primer commit

--- 2.py
+++ 2.py
@@ -1,6 +1,6 @@
 def perm(list,newPer):
     if not list:
-        print(newPer)    # Al final del árbol, envíe el resultado
+        print(newPer)    #  Al final del árbol, envíe el resultado
     else:               # Cuando no haya alcanzado el nodo hoja del árbol, use la recursividad para continuar mirando hacia abajo.
         for i in range(len(list)):
             newPer.append(list[i])
